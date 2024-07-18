b3c03bd0768072600a3f064fa980d104a9c1b234
flifez <flif3x@gmail.com>
2024-07-18 22:48:41 +0800

    chore: update .gitignore, _config.yml, special_thankyou.html, time_processor.js, embed_files.html, Gemfile, construction_template.html, and ibm_plex_mono.css

--- assets/js/time_processor.js
+++ assets/js/time_processor.js
@@ -3,8 +3,8 @@
 
 // This blank YAML front matter is required for Jekyll to process Liquid tags in JavaScript!
 
-let time = '{{ site.time }}'
-var raw = time.split(' ')
-var processed = raw[1].split(':')[0] + ':' + raw[1].split(':')[1]
+let time = '{{ site.time }}';
+var raw = time.split(' ');
+var processed = raw[1].split(':')[0] + ':' + raw[1].split(':')[1];
 
-document.write(processed)
+document.write(processed);
